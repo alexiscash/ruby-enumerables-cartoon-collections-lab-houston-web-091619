6581b541f285e376be2251ecaaa2ed5055199425
Alexis Rangel <alexiscash97@gmail.com>
2019-09-05 01:31:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.reduce { |memo, n| n.length <= 4 } 
+  array.reduce { |memo, n| memo || n.length <= 4 } 
 end
 
 def find_the_cheese# code an argument here
