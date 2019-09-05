c70bc2152f48bf3c3d9eff27f194cf35f2c51f3e
Alexis Rangel <alexiscash97@gmail.com>
2019-09-05 00:46:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(array)
   array.each_with_index { |elem, index| puts "#{index+1} #{elem}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map { |elem| elem.capitalize + '!' }
 end
 
 def long_planeteer_calls# code an argument here
@@ -17,4 +17,4 @@ end
 
 
 sample = ["Doc", "Dopey", "Bashful", "Grumpy"]
-roll_call_dwarves(sample)
+summon_captain_planet(sample)
